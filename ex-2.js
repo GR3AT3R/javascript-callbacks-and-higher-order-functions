261ee7cb82b14bc4b69cf6ca5363bf1a4abc8945
Gett <phonlachet.t@gmail.com>
2025-03-02 19:40:14 +0700

    Submit

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,17 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      count++;
+    }
+  }
+  return count >= 5 ? "ผ่านเกณฑ์ ✅" : "ไม่ผ่านเกณฑ์ ❌";
+}
+
+function isAbove70(score) {
+  return score > 70;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +21,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, isAbove70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, isAbove70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, isAbove70);
+
+console.log("นักเรียนห้องที่ 1 " + scoreRoom1Result);
+console.log("นักเรียนห้องที่ 2 " + scoreRoom2Result);
+console.log("นักเรียนห้องที่ 3 " + scoreRoom3Result);
